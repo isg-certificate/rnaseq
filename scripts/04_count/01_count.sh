52d1ba42e90f7c61eb94dc1d1aa5de6ed4694d6e
Noah Reid <nreid@hpc-ext-3.cam.uchc.edu>
2024-05-23 15:05:07 -0400

    more scripts

---
+++ scripts/04_count/01_count.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=htseq_count
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 10
+#SBATCH --mem=20G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+echo `hostname`
+date
+
+#################################################################
+# Generate Counts 
+#################################################################
+module load htseq/0.13.5
+module load parallel/20180122
+
+INDIR=../../03_align/alignments
+OUTDIR=../../04_counts/counts
+mkdir -p $OUTDIR
+
+# accession list
+ACCLIST=../../metadata/accessionlist.txt
+
+# gtf formatted annotation file
+GTF=../../genome/Fundulus_heteroclitus.Fundulus_heteroclitus-3.0.2.112.gtf 
+
+# run htseq-count on each sample, up to 5 in parallel
+cat $ACCLIST | \
+parallel -j 10 \
+    "htseq-count \
+        -s no \
+        -r pos \
+        -f bam $INDIR/{}.bam \
+        $GTF \
+        > $OUTDIR/{}.counts"
